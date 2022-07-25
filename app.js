0c0afb3212b27b194009d7b66973b828c657bd62
evisintin1 <evisintinmarquez@escuelasproa.edu.ar>
2022-07-25 14:27:52 -0300

    Init

---
+++ app.js
@@ -0,0 +1,42 @@
+//Todo lo relacionado con express (módulos, middlewares, configuraciones etc)
+
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const cloudinary = require("cloudinary").v2;
+const app = express();
+//const myRouter = require("./routes/myRouter");
+const cors = require("cors");
+//const session = require('express-session');
+//Defino el motor de plantillas a utilizar
+app.set("view engine", "ejs");
+//Defino la localización de mis vistas
+app.set("views", path.join(__dirname, "views"));
+
+app.use(cors());
+//Middlewares
+/*app.use(session({ 		//Usuage
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));*/
+//app.use(morgan("dev"));
+//Middleware para poder obtener data de los requests con BodyParser
+//app.use(express.json());
+//Configurando archivos estáticos
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+//Agrego un enrutador compatible
+//app.use("/", myRouter);
+//module.exports = app;
+const port = 4000;
+//Corremos el servidor en el puerto seleccionado
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port} correctamente`);
+});
+
+app.get('/',(req,res)=>{
+    res.render("index.ejs");
+})
