e01f76a2f5bf081ceb5757fc53f686bcf670234d
evisintin1 <evisintinmarquez@escuelasproa.edu.ar>
2022-07-25 14:36:19 -0300

    intento

--- app.js
+++ app.js
@@ -1,42 +1,10 @@
-//Todo lo relacionado con express (módulos, middlewares, configuraciones etc)
-
 const express = require("express");
-const path = require("path");
-const morgan = require("morgan");
-const cloudinary = require("cloudinary").v2;
 const app = express();
-//const myRouter = require("./routes/myRouter");
-const cors = require("cors");
-//const session = require('express-session');
-//Defino el motor de plantillas a utilizar
-app.set("view engine", "ejs");
-//Defino la localización de mis vistas
-app.set("views", path.join(__dirname, "views"));
+const product = require("./api/product");
 
-app.use(cors());
-//Middlewares
-/*app.use(session({ 		//Usuage
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
-}));*/
-//app.use(morgan("dev"));
-//Middleware para poder obtener data de los requests con BodyParser
-//app.use(express.json());
-//Configurando archivos estáticos
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json({ extended: false }));
 
-//Agrego un enrutador compatible
-//app.use("/", myRouter);
-//module.exports = app;
-const port = 4000;
-//Corremos el servidor en el puerto seleccionado
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port} correctamente`);
-});
+app.use("/api/product", product);
 
-app.get('/',(req,res)=>{
-    res.render("index.ejs");
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
